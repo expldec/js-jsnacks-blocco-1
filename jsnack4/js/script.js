34de6c4929ffff16d94240f85e5d4c52fa8823b5
expldec <explosive.deconstruction@gmail.com>
2022-04-19 13:58:41 +0200

    implemented all exercises

---
+++ jsnack4/js/script.js
@@ -0,0 +1,26 @@
+// jsnack4. Chiedi un numero di 4 cifre all'utente
+// e calcola la somma di tutte le cifre che compongono il numero.
+
+
+// dichiaro una stringa vuota
+let quattroCifre = "";
+
+// uso un do-while per assicurarmi che sia un numero intero di esattamente quattro cifre
+do {
+    quattroCifre = prompt("inserisci un numero di 4 cifre");
+    // se una di queste due condizioni è vera (per cui ho usato l'operatore OR ||), ripeti il loop.
+} while (quattroCifre.length != 4 || isNaN(parseInt(quattroCifre)))
+
+// creo un array contenente le singole cifre con lo string method ".split('')"
+let quattroCifreSplit = quattroCifre.split("");
+
+
+let somma = 0;
+
+//itero l'array e sommo i suoi componenti.
+for (let i = 0; i < quattroCifreSplit.length; i++) {
+    somma += parseInt(quattroCifreSplit[i])
+    
+}
+
+console.log(`il numero che hai scelto è ${quattroCifre}. La somma delle sue cifre (${quattroCifreSplit}) è ${somma}.`);
